c3c257e0aa43c948012cc1eaeae857851d11900f
Akinwunmi Lateef <MAC@MacBook-Air.local>
2026-03-13 23:19:58 +0300

    functionality of reset button added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const randomNumber = document.querySelector(".secret-number");
 
 const playGame = document.querySelector(".play-button");
+const resetButton = document.querySelector(".reset-button");
 //document.querySelector(".game-message");
 //document.querySelector(".current-score");
 //document.querySelector(".Highscore");
@@ -9,15 +10,17 @@ const playGame = document.querySelector(".play-button");
 let score = 20;
 let highScore = 0;
 let gamePlay = true;
+let guess;
 
-//console.log(randomNumber);
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+//generates the guess
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
+//plays the game
 playGame.addEventListener("click", function () {
-  const guess = Number(document.querySelector("#myvalue").value);
+  guess = Number(document.querySelector("#myvalue").value);
   console.log(guess);
+  console.log(secretNumber);
   if (gamePlay) {
     if (guess === secretNumber) {
       console.log(guess);
@@ -28,6 +31,7 @@ playGame.addEventListener("click", function () {
         highScore = score;
         document.querySelector(".Highscore").textContent = highScore;
       }
+
       gamePlay = false;
     } else if (guess < secretNumber && score > 0) {
       document.querySelector(".game-message").textContent = "Too low";
@@ -38,5 +42,19 @@ playGame.addEventListener("click", function () {
       score--;
       document.querySelector(".current-score").textContent = score;
     }
+    //guess.textContent = " ";
   }
+});
+//resets the game
+resetButton.addEventListener("click", function () {
+  gamePlay = true;
+  highScore = highScore;
+  document.querySelector(".game-message").textContent = "Start guessing...";
+  score = 20;
+  document.querySelector(".current-score").textContent = score;
+  randomNumber.textContent = "?";
+
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.899)";
 });
